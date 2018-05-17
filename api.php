4314fc815829b6f20311870eac7dc1dd93575c31
Sukirno <mblonyox@yahoo.co.id>
2018-05-17 10:23:05 +0700

    Tambahkan backend/cors proxy.

---
+++ api.php
@@ -0,0 +1,14 @@
+<?php
+// $url = 'https://9gag.com/v1/group-posts/group/default/type/hot';
+
+$host = 'https://9gag.com';
+$path = $_SERVER['PATH_INFO'];
+$query = http_build_query($_GET);
+
+// Get content
+$result = file_get_contents($host.$path.$query);
+
+// Output to client
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+echo $result;
