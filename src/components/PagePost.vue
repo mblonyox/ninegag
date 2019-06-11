085c51a4652e461935e7d4ea6e6881251fa697f3
mblonyox <mblonyox@gmail.com>
2019-06-11 21:07:53 +0700

    Fetch posts and programmatic navigation next post.

--- src/components/PagePost.vue
+++ src/components/PagePost.vue
@@ -6,6 +6,16 @@
         lg="8"
         class="px-0"
       >
+        <b-row align-h="between">
+          <b-col cols="2"><b-button block :to="{name: 'PageIndex'}">⮤ Home</b-button></b-col>
+          <b-col cols="2"><b-button block variant="primary" @click.prevent="nextPost">Next 🠚</b-button></b-col>
+        </b-row>
+        <card-content :post="post" v-if="!!post"/>
+        <b-card class="my-5" v-else>
+          <div class="text-center">
+            <b-spinner label="Loading..."/>
+          </div>
+        </b-card>
       </b-col>
     </b-row>
   </b-card>
@@ -16,8 +26,30 @@ import CardContent from './CardContent'
 
 export default {
   data: () => ({
-    post: {}
+    post: null,
+    nextPosts: []
   }),
+  methods: {
+    getPosts () {
+      this.post = null
+      return fetch('/api.php/v1/post?id=' + this.id)
+        .then(res => res.json())
+        .then(res => {
+          this.post = res.data.post
+          this.nextPosts = res.data.nextPosts
+        })
+    },
+    nextPost () {
+      (!this.nextPosts.length ? this.getPosts() : Promise.resolve())
+        .then(() => {
+          this.post = this.nextPosts.splice(0, 1)[0]
+          this.$router.push({name: 'PagePost', params: {id: this.post.id}})
+        })
+    }
+  },
+  created () {
+    this.getPosts()
+  },
   props: ['id'],
   components: {CardContent}
 }
