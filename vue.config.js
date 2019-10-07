bcbecd56f26ef388951f38c5131f77c6547b172b
Sukirno <sukirno89@kemenkeu.go.id>
2019-10-07 13:33:06 +0700

    Fix manifest icons.

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,13 @@ module.exports = {
     }
   },
   pwa: {
+    iconPaths: {
+      favicon32: 'img/icons/icon-32x32.png',
+      favicon16: 'img/icons/icon-16x16.png',
+      appleTouchIcon: 'img/icons/icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/icon-144x144.png'
+    },
     themeColor: "#212529",
     workboxOptions: {
       exclude: [
