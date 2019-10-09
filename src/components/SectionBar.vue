9c9419117658b1210477ed61bb49e651c4b756b8
mblonyox <mblonyox@gmail.com>
2019-10-09 21:59:01 +0700

    Add Popular category

--- src/components/SectionBar.vue
+++ src/components/SectionBar.vue
@@ -1,5 +1,16 @@
 <template>
   <b-list-group>
+    <b-list-group-item><b>POPULAR:</b></b-list-group-item>
+    <b-list-group-item
+      v-for="(type, key) in types"
+      :to="{name: 'PageIndexDefault', params: {type: key}}"
+      active-class="active"
+      :key="key"
+    >
+      <span style="min-width: 1.5em; display: inline-block;">{{type.icon}}</span>
+      {{type.name}}
+    </b-list-group-item>
+    <b-list-group-item><b>SECTIONS:</b></b-list-group-item>
     <b-list-group-item
       v-for="(section, key) in sections"
       :to="{name: 'PageIndexGroup', params: {group: section.url}}"
@@ -18,6 +29,22 @@ import { BListGroup, BListGroupItem } from 'bootstrap-vue';
 import { SectionMap } from '@/common/types';
 
 export default Vue.extend({
+  data: () => ({
+    types: {
+      hot: {
+        icon: '🔥',
+        name: 'Hot',
+      },
+      trending: {
+        icon: '📈',
+        name: 'Trending',
+      },
+      fresh: {
+        icon: '🕒',
+        name: 'Fresh',
+      },
+    },
+  }),
   computed: {
     sections(): SectionMap {
       return this.$store.state.sections;
